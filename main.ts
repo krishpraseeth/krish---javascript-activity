0970a701415076bbc26374e0232d37c878d47742
krishpraseeth <49354816+krishpraseeth@users.noreply.github.com>
2020-06-18 13:28:52 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+//String Examples:
+//Prints out name and your favorite color
+function nameAndColor(name:string , color:string){
+    console.log("Hello " + name + ". Your favorite color is " + color)
+}
+
+nameAndColor("Krish", "red")
+
+//Prints out name and your favorite number
+function nameAndNumber(name: string, favNum: number){
+    console.log("Hello " + name + ". Your favorite number is " + favNum) 
+}
+
+nameAndNumber("Krish", 24)  
+
